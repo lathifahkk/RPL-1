bd6f9b8e98262de913cf4a7855950dcc05420054
fid3101 <hafiduddin_31@apps.ipb.ac.id>
2020-05-05 16:33:04 +0700

    full welcome page commit

--- app/src/main/java/com/example/elis/MainActivity.java
+++ app/src/main/java/com/example/elis/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.elis;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Handler;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT =3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent (MainActivity.this,homeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
